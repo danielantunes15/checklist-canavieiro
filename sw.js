b6e43696f1bf80c16955b16ef0133ae384fc998c
danielantunes15 <danielantunes.dev@gmail.com>
2025-12-18 21:47:19 -0300

    atualizei

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (e) => {
+  console.log('Service Worker instalado');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Necessário para o PWA ser reconhecido
+  e.respondWith(fetch(e.request));
+});
